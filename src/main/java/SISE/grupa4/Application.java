1ed032528c98c3855d243829f221b33b27d01678
unknown <piotr1q2w@gmail.com>
2021-04-03 15:07:27 +0200

    setting up basic javafx application

--- src/main/java/SISE/grupa4/Application.java
+++ src/main/java/SISE/grupa4/Application.java
@@ -1,7 +1,24 @@
 package SISE.grupa4;
 
-public class Application {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("MainStage.fxml")));
+
+        stage.setTitle("Pietnastka");
+        stage.setScene(new Scene(root, 1000, 600));
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch();
     }
 }
